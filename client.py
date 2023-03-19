221818eea7995a1437fa09b0ba55bd362292abcd
vincentyono <vincentyono@protonmail.com>
2023-03-19 14:20:08 +0700

    Implement class Client

--- client.py
+++ client.py
@@ -1,42 +1,16 @@
-import requests
-import json
-import socket
+from socket import AF_INET, SOCK_DGRAM, socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#IP and port of the server
-ip = input('Enter server IP address: ')
-port = int(input('Enter server PORT: '))
+class Client:
+    def __init__(self):
+        self.__socket = socket(AF_INET, SOCK_DGRAM)
 
-#Loop for asking inputs
-while True:
-    cmd = input('Enter "get", "msg" or "post" command (or "exit" to quit): ')
-    if cmd == 'exit':
-        break
-    #GET method 
-    if cmd == 'get':
-        query = input("What do you want ?: ")
-        sock.sendto(("I WANT " + query).encode(), (ip, port))
-        #receives 1024 bytes in socketserver
-        data, addr = sock.recvfrom(1024)
-        print(data)
-    
-    #P2P messaging
-    if cmd == 'msg':
-        query = input("Input message: ")
-        sock.sendto(query.encode(), (ip, port))
-        data, addr = sock.recvfrom(1024)
-        print(data)
+    def send(self, data: str, ip_address: str, port: int):
+        self.__socket.sendto(data.encode(), (ip_address, port))
 
-    #POST method
-    elif cmd == 'post':
-        hd = input("Head of message: ")
-        msg = input("Message to be sent: ")
-        #JSON file format
-        payload = json.dumps({"head": hd, "message": msg})
-        headers = '{"Content-Type":"application/json"}'
-        #Send payload to server
-        response = sock.sendto(("PUTTING" + " \n " +str(headers) + " \n " + str(payload)).encode(), (ip, port))
+    def receive(self, buffer_size: int = 1024):
+        message, server_address = self.__socket.recvfrom(buffer_size)
+        print(f"Sending to {server_address}: {message.decode()}")
 
-    else:
-        print('Invalid command')
+    def close(self):
+        self.__socket.close()
